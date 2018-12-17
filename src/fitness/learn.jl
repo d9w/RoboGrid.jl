ec80008b80f783279e08711b07faa9e0263f0033
Dennis Wilson <dennis@d9w.xyz>
2018-12-17 11:31:09 +0100

    reward latest search in learning fitness

--- src/fitness/learn.jl
+++ src/fitness/learn.jl
@@ -1,14 +1,19 @@
-function learn_fitness(cont_f::Function, map_f::Function, meta_f::Function)
-    steps = Array{Int64}(undef, 6)
+function learn_fitness(cont_f::Function, map_f::Function, meta_f::Function;
+                       neps::Int64=6)
+    steps = Array{Int64}(undef, neps)
     max_step = -Inf
-    for i in 1:6
+    for i in 1:neps
         e = Episode(Grid(map_f()); reward=food_reward, meta=meta_f())
         run!(e, cont_f)
         if terminate(e)
-            return i - 7.0
+            if i == 1
+                return Float64(-neps)
+            else
+                return Float64(i - neps - steps[i-1] / e.meta["max_step"])
+            end
         end
         steps[i] = e.meta["step"]
         max_step = e.meta["max_step"]
     end
-    (steps[1] - steps[6]) / max_step
+    (steps[1] - steps[neps]) / max_step
 end
